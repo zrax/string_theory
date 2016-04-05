9b90b0a310cac26bc0baa0e1a20d3d1b9afcfa2d
Michael Hansen <zrax0111@gmail.com>
2016-04-05 15:42:18 -0700

    Cleanup unused variable warnings

--- test/profile.cpp
+++ test/profile.cpp
@@ -37,6 +37,9 @@
 #   define M_PI (3.14159265358979)
 #endif
 
+volatile const char *V;
+#define NO_OPTIMIZE(x) V = (x)
+
 static void _measure(const char *title, std::function<void()> fun)
 {
     auto clk = std::chrono::high_resolution_clock::now();
@@ -54,103 +57,103 @@ int main(int, char **)
 
     _measure("Empty std::string", []() {
         std::string s;
-        volatile const char *V = s.c_str();
+        NO_OPTIMIZE(s.c_str());
     });
 
     _measure("Empty ST::string", []() {
         ST::string s;
-        volatile const char *V = s.c_str();
+        NO_OPTIMIZE(s.c_str());
     });
 
     _measure("Short std::string", []() {
         std::string short_str("Short");
-        volatile const char *V = short_str.c_str();
+        NO_OPTIMIZE(short_str.c_str());
     });
 
     _measure("Short ST::string", []() {
         ST::string short_str("Short");
-        volatile const char *V = short_str.c_str();
+        NO_OPTIMIZE(short_str.c_str());
     });
 
     _measure("Short ST::string (assume_valid)", []() {
         ST::string short_str("Short", ST_AUTO_SIZE, ST::assume_valid);
-        volatile const char *V = short_str.c_str();
+        NO_OPTIMIZE(short_str.c_str());
     });
 
     _measure("Short ST::string (literal)", []() {
         ST::string short_str = ST_LITERAL("Short");
-        volatile const char *V = short_str.c_str();
+        NO_OPTIMIZE(short_str.c_str());
     });
 
     _measure("Long std::string", []() {
         std::string long_str("This is a long string.  Testing the excessively long long string.");
-        volatile const char *V = long_str.c_str();
+        NO_OPTIMIZE(long_str.c_str());
     });
 
     _measure("Long ST::string", []() {
         ST::string long_str("This is a long string.  Testing the excessively long long string.");
-        volatile const char *V = long_str.c_str();
+        NO_OPTIMIZE(long_str.c_str());
     });
 
     _measure("Long ST::string (assume_valid)", []() {
         ST::string long_str("This is a long string.  Testing the excessively long long string.",
                             ST_AUTO_SIZE, ST::assume_valid);
-        volatile const char *V = long_str.c_str();
+        NO_OPTIMIZE(long_str.c_str());
     });
 
     _measure("Long ST::string (literal)", []() {
         ST::string long_str = ST_LITERAL("This is a long string.  Testing the excessively long long string.");
-        volatile const char *V = long_str.c_str();
+        NO_OPTIMIZE(long_str.c_str());
     });
 
     std::string _xc1("Short");
     _measure("Copy short std::string", [&_xc1]() {
         std::string copy = _xc1;
-        volatile const char *V = copy.c_str();
+        NO_OPTIMIZE(copy.c_str());
     });
 
     ST::string _xc2("Short");
     _measure("Copy short ST::string", [&_xc2]() {
         ST::string copy = _xc2;
-        volatile const char *V = copy.c_str();
+        NO_OPTIMIZE(copy.c_str());
     });
 
     std::string _xc3("This is a long string.  Testing the excessively long long string.");
     _measure("Copy long std::string", [&_xc3]() {
         std::string copy = _xc3;
-        volatile const char *V = copy.c_str();
+        NO_OPTIMIZE(copy.c_str());
     });
 
     ST::string _xc4("This is a long string.  Testing the excessively long long string.");
     _measure("Copy long ST::string", [&_xc4]() {
         ST::string copy = _xc4;
-        volatile const char *V = copy.c_str();
+        NO_OPTIMIZE(copy.c_str());
     });
 
     _measure("static snprintf", []() {
         char buffer[256];
         snprintf(buffer, 256, "This %d is %6.2f a %s test %c.", 42, M_PI, "<Singin' in the rain>", '?');
-        volatile const char *V = buffer;
+        NO_OPTIMIZE(buffer);
     });
 
     _measure("dynamic snprintf", []() {
         int len = snprintf(nullptr, 0, "This %d is %6.2f a %s test %c.", 42, M_PI, "<Singin' in the rain>", '?');
         char *buffer = new char[len + 1];
         snprintf(buffer, len, "This %d is %6.2f a %s test %c.", 42, M_PI, "<Singin' in the rain>", '?');
-        volatile const char *V = buffer;
+        NO_OPTIMIZE(buffer);
         delete[] buffer;
     });
 
     _measure("ST::format", []() {
         ST::string foo = ST::format("This {} is {6.2f} a {} test {}.", 42, M_PI, "<Singin' in the rain>", '?');
-        volatile const char *V = foo.c_str();
+        NO_OPTIMIZE(foo.c_str());
     });
 
 #ifdef ST_PROFILE_HAVE_BOOST
     _measure("boost::format", []() {
         std::string foo = (boost::format("This %1% is %2$6.2f a %3% test %4%.")
                            % 42 % M_PI % "<Singin' in the rain>" % '?').str();
-        volatile const char *V = foo.c_str();
+        NO_OPTIMIZE(foo.c_str());
     });
 #endif
 
@@ -158,14 +161,14 @@ int main(int, char **)
         std::stringstream ss;
         ss << "This " << 42 << " is " << M_PI << " a "
            << "<Singin' in the rain>" << " test " << '?' << ".";
-        volatile const char *V = ss.str().c_str();
+        NO_OPTIMIZE(ss.str().c_str());
     });
 
     _measure("ST::string_stream (~format)", []() {
         ST::string_stream ss;
         ss << "This " << 42 << " is " << M_PI << " a "
            << "<Singin' in the rain>" << " test " << '?' << ".";
-        volatile const char *V = ss.to_string().c_str();
+        NO_OPTIMIZE(ss.to_string().c_str());
     });
 
     return 0;
