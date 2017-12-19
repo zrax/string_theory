8cb95c02e5be72c79fae702667366a4df1f5bc13
Michael Hansen <zrax0111@gmail.com>
2017-12-19 13:13:05 -0800

    Add more profile cases

--- test/profile.cpp
+++ test/profile.cpp
@@ -32,9 +32,11 @@
 
 #ifdef ST_PROFILE_HAVE_BOOST
 #   include <boost/format.hpp>
+#   include <boost/algorithm/string.hpp>
 #endif
 #ifdef ST_PROFILE_HAVE_QSTRING
 #   include <QString>
+#   include <QStringList>
 #endif
 #ifdef ST_PROFILE_HAVE_GLIBMM
 #   include <glibmm/ustring.h>
@@ -50,6 +52,9 @@
 
 volatile const char *V;
 #define NO_OPTIMIZE(x) V = reinterpret_cast<const char *>(x)
+
+volatile long L;
+#define NO_OPTIMIZE_L(x) L = x;
 
 static void _measure(const char *title, std::function<void()> fun)
 {
@@ -79,7 +84,7 @@ int main(int, char **)
 #ifdef ST_PROFILE_HAVE_QSTRING
     _measure("Empty QString", []() {
         QString s;
-        NO_OPTIMIZE(s.data());
+        NO_OPTIMIZE(s.constData());
     });
 #endif
 
@@ -89,6 +94,8 @@ int main(int, char **)
         NO_OPTIMIZE(s.c_str());
     });
 #endif
+
+    ST::printf("\n");
 
     _measure("Short std::string", []() {
         std::string short_str("Short");
@@ -113,12 +120,12 @@ int main(int, char **)
 #ifdef ST_PROFILE_HAVE_QSTRING
     _measure("Short QString", []() {
         QString short_str("Short");
-        NO_OPTIMIZE(short_str.data());
+        NO_OPTIMIZE(short_str.constData());
     });
 
     _measure("Short QString (literal)", []() {
         QString short_str = QStringLiteral("Short");
-        NO_OPTIMIZE(short_str.data());
+        NO_OPTIMIZE(short_str.constData());
     });
 #endif
 
@@ -128,6 +135,8 @@ int main(int, char **)
         NO_OPTIMIZE(short_str.c_str());
     });
 #endif
+
+    ST::printf("\n");
 
     _measure("Long std::string", []() {
         std::string long_str("This is a long string.  Testing the excessively long long string.");
@@ -153,12 +162,12 @@ int main(int, char **)
 #ifdef ST_PROFILE_HAVE_QSTRING
     _measure("Long QString", []() {
         QString long_str("This is a long string.  Testing the excessively long long string.");
-        NO_OPTIMIZE(long_str.data());
+        NO_OPTIMIZE(long_str.constData());
     });
 
     _measure("Long QString (literal)", []() {
         QString long_str = QStringLiteral("This is a long string.  Testing the excessively long long string.");
-        NO_OPTIMIZE(long_str.data());
+        NO_OPTIMIZE(long_str.constData());
     });
 #endif
 
@@ -168,6 +177,8 @@ int main(int, char **)
         NO_OPTIMIZE(long_str.c_str());
     });
 #endif
+
+    ST::printf("\n");
 
     std::string _ss1("Short");
     _measure("Copy short std::string", [&_ss1]() {
@@ -185,7 +196,7 @@ int main(int, char **)
     QString _qs1("Short");
     _measure("Copy short QString", [&_qs1]() {
         QString copy = _qs1;
-        NO_OPTIMIZE(copy.data());
+        NO_OPTIMIZE(copy.constData());
     });
 #endif
 
@@ -196,6 +207,8 @@ int main(int, char **)
         NO_OPTIMIZE(copy.c_str());
     });
 #endif
+
+    ST::printf("\n");
 
     std::string _ss2("This is a long string.  Testing the excessively long long string.");
     _measure("Copy long std::string", [&_ss2]() {
@@ -213,7 +226,7 @@ int main(int, char **)
     QString _qs2("This is a long string.  Testing the excessively long long string.");
     _measure("Copy long QString", [&_qs2]() {
         QString copy = _qs2;
-        NO_OPTIMIZE(copy.data());
+        NO_OPTIMIZE(copy.constData());
     });
 #endif
 
@@ -224,6 +237,8 @@ int main(int, char **)
         NO_OPTIMIZE(copy.c_str());
     });
 #endif
+
+    ST::printf("\n");
 
     std::string _ss3[] = {"Piece 1", "Piece 2", "Piece 3"};
     _measure("std::string (+)", [&_ss3]() {
@@ -241,7 +256,7 @@ int main(int, char **)
     QString _qs3[] = {"Piece 1", "Piece 2", "Piece 3"};
     _measure("QString (+)", [&_qs3]() {
         QString result = _qs3[0] + _qs3[1] + _qs3[2];
-        NO_OPTIMIZE(result.data());
+        NO_OPTIMIZE(result.constData());
     });
 #endif
 
@@ -252,6 +267,136 @@ int main(int, char **)
         NO_OPTIMIZE(result.c_str());
     });
 #endif
+
+    ST::printf("\n");
+
+    const char *_is1 = "5143200";
+    _measure("strtol", [&_is1]() {
+        long result = strtol(_is1, nullptr, 10);
+        NO_OPTIMIZE_L(result);
+    });
+
+    ST::string _ist1 = "5143200";
+    _measure("ST::string::to_int", [&_ist1]() {
+        long result = _ist1.to_int(10);
+        NO_OPTIMIZE_L(result);
+    });
+
+#ifdef ST_PROFILE_HAVE_QSTRING
+    QString _iqs1 = "5143200";
+    _measure("QString::toInt", [&_iqs1]() {
+        long result = _iqs1.toInt(ST_NULLPTR, 10);
+        NO_OPTIMIZE_L(result);
+    });
+#endif
+
+    ST::printf("\n");
+
+    int _ival = 5143200;
+    _measure("sprintf number", [_ival]() {
+        char buffer[16];
+        snprintf(buffer, 16, "%d", _ival);
+        NO_OPTIMIZE(buffer);
+    });
+
+    _measure("std::to_string", [_ival]() {
+        std::string result = std::to_string(_ival);
+        NO_OPTIMIZE(result.c_str());
+    });
+
+    _measure("ST::from_int", [_ival]() {
+        ST::string result = ST::string::from_int(_ival);
+        NO_OPTIMIZE(result.c_str());
+    });
+
+#ifdef ST_PROFILE_HAVE_QSTRING
+    _measure("QString::number", [_ival]() {
+        QString result = QString::number(_ival);
+        NO_OPTIMIZE(result.constData());
+    });
+#endif
+
+#ifdef ST_PROFILE_HAVE_GLIBMM
+    _measure("Glib::ustring::format", [_ival]() {
+        Glib::ustring result = Glib::ustring::format(_ival);
+        NO_OPTIMIZE(result.c_str());
+    });
+#endif
+
+    ST::printf("\n");
+
+    std::string _ss4 = "One|Two|Part Three is much longer than the others|Part Four";
+    _measure("std::getline", [&_ss4]() {
+        std::vector<std::string> results;
+        std::stringstream ss(_ss4);
+        std::string item;
+        while (std::getline(ss, item, '|'))
+            results.push_back(item);
+        NO_OPTIMIZE(results[0].c_str());
+        NO_OPTIMIZE(results[1].c_str());
+        NO_OPTIMIZE(results[2].c_str());
+        NO_OPTIMIZE(results[3].c_str());
+    });
+
+    ST::string _st4 = "One|Two|Part Three is much longer than the others|Part Four";
+    _measure("ST::string::split", [&_st4]() {
+        std::vector<ST::string> results = _st4.split('|');
+        NO_OPTIMIZE(results[0].c_str());
+        NO_OPTIMIZE(results[1].c_str());
+        NO_OPTIMIZE(results[2].c_str());
+        NO_OPTIMIZE(results[3].c_str());
+    });
+
+#ifdef ST_PROFILE_HAVE_BOOST
+    _measure("boost::split", [&_ss4]() {
+        std::vector<std::string> results;
+        boost::split(results, _ss4, boost::is_any_of("|"));
+        NO_OPTIMIZE(results[0].c_str());
+        NO_OPTIMIZE(results[1].c_str());
+        NO_OPTIMIZE(results[2].c_str());
+        NO_OPTIMIZE(results[3].c_str());
+    });
+#endif
+
+#ifdef ST_PROFILE_HAVE_QSTRING
+    QString _qs4 = "One|Two|Part Three is much longer than the others|Part Four";
+    _measure("QString::split", [&_qs4]() {
+        QStringList results = _qs4.split('|');
+        NO_OPTIMIZE(results[0].constData());
+        NO_OPTIMIZE(results[1].constData());
+        NO_OPTIMIZE(results[2].constData());
+        NO_OPTIMIZE(results[3].constData());
+    });
+#endif
+
+    ST::printf("\n");
+
+    _measure("std::string::substr", [&_ss4]() {
+        std::string sub = _ss4.substr(8, 41);
+        NO_OPTIMIZE(sub.c_str());
+    });
+
+    _measure("ST::string::substr", [&_st4]() {
+        ST::string sub = _st4.substr(8, 41);
+        NO_OPTIMIZE(sub.c_str());
+    });
+
+#ifdef ST_PROFILE_HAVE_QSTRING
+    _measure("QString::mid", [&_qs4]() {
+        QString sub = _qs4.mid(8, 41);
+        NO_OPTIMIZE(sub.constData());
+    });
+#endif
+
+#ifdef ST_PROFILE_HAVE_GLIBMM
+    Glib::ustring _gs4 = "One|Two|Part Three is much longer than the others|Part Four";
+    _measure("Glib::ustring::substr", [&_gs4]() {
+        Glib::ustring sub = _gs4.substr(8, 41);
+        NO_OPTIMIZE(sub.c_str());
+    });
+#endif
+
+    ST::printf("\n");
 
     _measure("static snprintf", []() {
         char buffer[256];
@@ -288,7 +433,7 @@ int main(int, char **)
     _measure("QString::arg", []() {
         QString foo = QString("This %1 is %2 a %3 test %4.")
                       .arg(42).arg(M_PI, 6, 'f', 2).arg("<Singin' in the rain>").arg('?');
-        NO_OPTIMIZE(foo.data());
+        NO_OPTIMIZE(foo.constData());
     });
 #endif
 
@@ -299,6 +444,8 @@ int main(int, char **)
         NO_OPTIMIZE(foo.c_str());
     });
 #endif
+
+    ST::printf("\n");
 
 #ifdef _WIN32
 #   define DEVNULL "nul"
