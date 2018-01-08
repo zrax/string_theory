454030031ceeed78ed1c58b19b16a0a7fbd23798
Michael Hansen <zrax0111@gmail.com>
2018-01-07 17:04:34 -0800

    These should be in the _ST_PRIVATE namespace, but MSVC 2013 apparently can't handle it and throws an Internal Compiler Error when they are.  :(

--- include/st_formatter_util.h
+++ include/st_formatter_util.h
@@ -24,29 +24,26 @@
 #include "st_formatter.h"
 #include <functional>
 
-namespace _ST_PRIVATE
+namespace ST
 {
-    typedef std::function<void (const ST::format_spec &, ST::format_writer &)>
-        formatter_ref_t;
+    typedef std::function<void(const ST::format_spec &, ST::format_writer &)>
+        _formatter_ref_t;
 
     template <typename type_T>
-    formatter_ref_t make_formatter_ref(type_T value)
+    _formatter_ref_t _make_formatter_ref(type_T value)
     {
         return [value](const ST::format_spec &format, ST::format_writer &output) {
             ST_INVOKE_FORMATTER(format, output, value);
         };
     }
-}
 
-namespace ST
-{
     template <typename arg0_T, typename... args_T>
     void apply_format(ST::format_writer &data, arg0_T &&arg0, args_T &&...args)
     {
         enum { num_formatters = 1 + sizeof...(args) };
-        _ST_PRIVATE::formatter_ref_t formatters[num_formatters] = {
-            _ST_PRIVATE::make_formatter_ref(std::forward<arg0_T>(arg0)),
-            _ST_PRIVATE::make_formatter_ref(std::forward<args_T>(args))...
+        _formatter_ref_t formatters[num_formatters] = {
+            _make_formatter_ref(std::forward<arg0_T>(arg0)),
+            _make_formatter_ref(std::forward<args_T>(args))...
         };
         size_t index = 0;
         while (data.next_format()) {
